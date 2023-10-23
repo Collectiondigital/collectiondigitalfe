a5c7a554b7350b7c196791296615b1546a40eef9
noxtros <mr.kevinkalus@gmail.com>
2023-10-23 09:11:32 +0200

    searchbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
     <>
-      <button>Login</button>
+      <button className="login_button">Login</button>
     </>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,52 @@
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+export default function Searchbar() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
 
-export default function Search() {
+  const API = `https://api.vam.ac.uk/v2/objects/search?q=${query}&page=1&page_size=15`;
+
+  const fetchData = async () => {
+    const res = await fetch(API);
+    const data = await res.json();
+    console.log("DATA: ", data.records);
+    setData(data.records);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query]);
+
+  console.log("QUERY: ", query);
+
   return (
-    <>
-      <div className="search">
-        <FaSearch id="search_icon" />
-        <input
-          className="search_bar"
-          placeholder="Type here for your search..."
-        />
-        <button className="submit" type="submit">
-          Search
-        </button>
+    <div>
+      <label style={{ margin: "0.4em" }}>
+        <FaSearch />
+      </label>
+      <input
+        className="searcher"
+        placeholder="Type here to search ..."
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <br />
+      <div className="searched">
+        {data
+          ? data.map((record) => (
+              <div key={record.accessionNumber} className="results">
+                <h5>
+                  {record._primaryTitle ? record._primaryTitle : "No name"}
+                </h5>
+                <img
+                  className="result_pic"
+                  src={`${record._images._iiif_image_base_url}/full/900,/0/default.jpg`}
+                />
+              </div>
+            ))
+          : null}
       </div>
-    </>
+    </div>
   );
 }
