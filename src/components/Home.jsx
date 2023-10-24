d69df52ca1c525acc7ec6e17dbbaa3be9ffd7c89
noxtros <mr.kevinkalus@gmail.com>
2023-10-24 10:58:49 +0200

    Home styling, search result imagelink

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,65 @@
+import "./CSS/Home.css";
+
 export default function Home() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          borderBottom: "1px solid grey",
+          paddingBottom: "2%",
+          color: "white",
+        }}
+      >
+        <h1 style={{ marginBottom: "2%" }}>Welcome to Collection Digital</h1>
+        <h4>Share your passion for collecting with others</h4>
+      </div>
 
+      <h3 style={{ marginTop: "2%", marginBottom: "2%", color: "white" }}>
+        What we offer
+      </h3>
 
+      <div className="test">
+        <div className="homepage_card">
+          <div className="homepage_h5">
+            <h5>Document</h5>
+            <p>
+              Store your collection details and images in one place with access
+              from desktop, tablet and mobile.
+            </p>
+          </div>
+        </div>
 
+        <div className="homepage_card">
+          <div className="homepage_h5">
+            <h5>Manage</h5>
+            <p>
+              Easy to use, affordable and scalable as your collection changes.
+            </p>
+          </div>
+        </div>
+        <div className="homepage_card">
+          <div className="homepage_h5">
+            <h5>Share</h5>
+            <p>
+              Share your collection with others via social media using
+              integrated links.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h3 style={{ marginTop: "2%", marginBottom: "2%", color: "white" }}>
+        Offer
+      </h3>
 
-    return (
-        <>
-            {/* Navbar */}
-            {/* Searchbar */}
-            <div>
-                <h1>Welcome to Collection Digital</h1>
-                <h4>Share your passion for collecting with others</h4>
-                <hr />
-            </div>
-
-            <h3>What we offer</h3>
-
-            <div className="homepage_card">
-                <div>
-                    <h5>Document</h5>
-                    <p>Store your collection details and images in one place
-                        with access from desktop, tablet and mobile.</p>
-                </div>
-            </div>
-
-            <div className="homepage_card">
-                <div>
-                    <h5>Manage</h5>
-                    <p>Easy to use, affordable and scalable as your collection changes.</p>
-                </div>
-
-            </div>
-            <div className="homepage_card">
-                <div>
-                    <h5>Share</h5>
-                    <p>Share your collection with others via social media
-                        using integrated links.</p>
-                </div>
-            </div>
-
-            <hr />
-
-            <h3>What we offer</h3>
-
-            <div className="new_collections_card">
-                <img src="" alt="" />
-                <h5>name of collection</h5>
-                <p>short description of collection</p>
-            </div>
-        </>
-
-    );
+      <div className="new_collections_card">
+        <img src="" alt="" />
+        <h5>name of collection</h5>
+        <p>short description of collection</p>
+      </div>
+    </>
+  );
 }
-
