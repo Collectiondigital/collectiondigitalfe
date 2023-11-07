a6e0042b75ce9951fd48e5269ee076b8aba91da4
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-11-07 17:01:47 +0100

    adding feateres

--- src/components/CollItemPage.jsx
+++ src/components/CollItemPage.jsx
@@ -3,39 +3,34 @@ import { CollectionsContext } from "../context/collectionsContext";
 import { useContext, useState } from "react";
 import "./CSS/CollItemPage.css";
 
-
 export default function CollItemPage() {
-    const { collections } = useContext(CollectionsContext);
-    console.log("collections inside item page", collections);
-    const { id } = useParams();
+  const { collections } = useContext(CollectionsContext);
+  console.log("collections inside item page", collections);
+  const { id } = useParams();
 
-    let foundItem = null;
+  let foundItem = null;
 
-    for (const collection of collections) {
-        if (collection.items && Array.isArray(collection.items)) {
-            for (const item of collection.items) {
-                if (item._id === id) {
-                    foundItem = item;
-                    break;
-                }
-            }
+  for (const collection of collections) {
+    if (collection.items && Array.isArray(collection.items)) {
+      for (const item of collection.items) {
+        if (item._id === id) {
+          foundItem = item;
+          break;
         }
-
-        if (foundItem) {
-            break;
-        }
+      }
     }
 
-    console.log(foundItem);
+    if (foundItem) {
+      break;
+    }
+  }
 
-    return (
+  console.log(foundItem);
 
-        <div>
-            
-            <h1>Object Type: {foundItem.object_type}</h1>
-            
-        </div>
-
-
-    );
-} 
+  return (
+    <div>
+      <h1>Object Type: {foundItem.object_type}</h1>
+      <h1>Second Type: {foundItem.object_type}</h1>
+    </div>
+  );
+}
