0b2d7a50002a0e7b6a8bce664170cea7593b8382
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-11-07 17:55:41 +0100

    collection iterable

--- src/components/CollItemPage.jsx
+++ src/components/CollItemPage.jsx
@@ -10,36 +10,33 @@ export default function CollItemPage() {
 
   let foundItem = null;
 
-  for (const collection of collections) {
-    if (collection.items && Array.isArray(collection.items)) {
-      for (const item of collection.items) {
-        if (item._id === id) {
-          foundItem = item;
-          break;
+  if (collections) {
+    for (const collection of collections) {
+      if (collection.items && Array.isArray(collection.items)) {
+        for (const item of collection.items) {
+          if (item._id === id) {
+            foundItem = item;
+            break;
+          }
         }
       }
+
+      if (foundItem) {
+        break;
+      }
     }
+  }
 
-
-        if (foundItem) {
-            break;
-        }
-    }
-
-    const navigate= useNavigate();
+  const navigate = useNavigate();
 
   console.log(foundItem);
 
-    return (
-
-        <div>
-            <span onClick={() => navigate(-1)}>
-                <img className="arrow-image" src="/arrow-back.png" alt="back" />
-            </span>
-            <h1>Object Type: {foundItem.object_type}</h1>
-
-        </div>
-
-
-    );
-} 
+  return (
+    <div>
+      <span onClick={() => navigate(-1)}>
+        <img className="arrow-image" src="/arrow-back.png" alt="back" />
+      </span>
+      <h1>Object Type: {foundItem.object_type}</h1>
+    </div>
+  );
+}
